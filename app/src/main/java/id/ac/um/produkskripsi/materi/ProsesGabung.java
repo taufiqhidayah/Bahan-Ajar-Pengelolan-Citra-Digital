d77b88ea8756de6703453d7ad1c1f0afaa9177d5
arifmh18 <firearif007@gmail.com>
2018-03-23 21:32:30 +0700

    Penambahan Fitur List Data

---
+++ app/src/main/java/id/ac/um/produkskripsi/materi/ProsesGabung.java
@@ -0,0 +1,13 @@
+package id.ac.um.produkskripsi.materi;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ProsesGabung extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_proses_gabung);
+    }
+}
